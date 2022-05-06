7f11550b173c834488cad8fd00a5fa2d0bfe1538
Dockerizer for PHP <email@example.com>
2022-05-06 13:58:54 +0300

    task 10 - reformatted to jQuery

--- app/design/frontend/Vlad/Skeleton/Magento_Catalog/web/js/qty-controls.js
+++ app/design/frontend/Vlad/Skeleton/Magento_Catalog/web/js/qty-controls.js
@@ -5,21 +5,48 @@ define(["jquery"], function ($) {
         },
         _initQty: function () {
 
-            var buttonPlus = document.querySelector('.qty__button__increment'),
-                buttonMinus = document.querySelector('.qty__button__decrement'),
-                elCurrentQty = document.querySelector('#qty');
+            // jQuery
 
-            buttonPlus.addEventListener('click', (e) => {
+            var buttonPlus = $(this.element).find('.qty__button__increment'),
+                buttonMinus = $(this.element).find('.qty__button__decrement'),
+                elCurrentQty = $(this.element).find('input.qty');
+
+            buttonPlus.on('click', (e) => {
                 e.preventDefault();
-                elCurrentQty.value = parseInt(elCurrentQty.value) + 1;
+                elCurrentQty.val(parseInt(elCurrentQty.val()) + 1);
+
             })
 
-            buttonMinus.addEventListener('click', (e) => {
+            buttonMinus.on('click', (e) => {
                 e.preventDefault();
-                if (elCurrentQty.value > 1) {
-                    elCurrentQty.value = parseInt(elCurrentQty.value) - 1;
+                if (elCurrentQty.val() > 1) {
+                    elCurrentQty.val(parseInt(elCurrentQty.val()) - 1);
                 }
-            });
+            })
         }
     })
-});
+})
+
+//             JavaScript
+
+//             var buttonPlus = document.querySelector('.qty__button__increment'),
+//                 buttonMinus = document.querySelector('.qty__button__decrement'),
+//                 elCurrentQty = document.querySelector('#qty');
+//
+//             buttonPlus.addEventListener('click', (e) => {
+//                 e.preventDefault();
+//                 elCurrentQty.value = parseInt(elCurrentQty.value) + 1;
+//             })
+//
+//             buttonMinus.addEventListener('click', (e) => {
+//                 e.preventDefault();
+//                 if (elCurrentQty.value > 1) {
+//                     elCurrentQty.value = parseInt(elCurrentQty.value) - 1;
+//                 }
+//             });
+//         }
+//     })
+// });
+
+
+
